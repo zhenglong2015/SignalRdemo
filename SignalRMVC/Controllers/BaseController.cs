fd673c8d426df469291f4058cde689e6d2310cca
郑隆 <853222835@qq.com>
2018-12-27 22:30:25 +0800

    增加SignalR和MVC结合案例

---
+++ SignalRMVC/Controllers/BaseController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SignalRMVC.Controllers
+{
+    /// <summary>
+    /// 控制器基类
+    /// </summary>
+    /// <typeparam name="T">Hub</typeparam>
+    public class BaseController<T> : Controller where T : Hub
+    {
+        public IHubConnectionContext<dynamic> Clients { get; set; }
+
+        public IGroupManager Groups { get; set; }
+
+        public BaseController()
+        {
+            var gh = GlobalHost.ConnectionManager.GetHubContext<T>();
+
+            Clients = gh.Clients;
+
+            Groups = gh.Groups;
+        }
+
+    }
+}
---
+++ SignalRMVC/Controllers/DefaultController.cs
@@ -0,0 +1,18 @@
+﻿using Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SignalRMVC.Controllers
+{
+    public class DefaultController : BaseController<MyHub>
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
